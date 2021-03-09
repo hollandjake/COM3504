b4f07da07eb7210e0b2386a467415852c86f4959
tomboyd <tom.boyd@icabbi.com>
2021-03-09 13:00:19 +0000

    Login Page HTML/CSS Closes #2

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,17 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Image Browsing' });
+
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+/* POST login page. */
+router.post('/login', function(req, res, next) {
+  res.redirect('/');
 });
 
 module.exports = router;
