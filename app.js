1520ef73a9610a6031e8d99cc3344258d0786b23
Jake Holland <hollandjake@hotmail.co.uk>
2021-03-09 13:04:47 +0000

    Implemented index page
    - Added '/' page Closes #1
    - Added '/job/list' api endpoint to load all the jobs
    - Added support for partial templates through ejs

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var jobRouter   = require('./routes/job');
 
 var app = express();
 
@@ -18,11 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
-app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/job', jobRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ public/javascripts/index/index.js
@@ -0,0 +1,45 @@
+// On load
+$(function () {
+    $.ajax({
+        type: 'get',
+        url: '/job/list',
+        success: function (jobsData) {
+            console.log(jobsData);
+            let jobListElement = $('#job-list-container');
+
+            jobListElement.empty(); //Remove the child nodes
+
+            jobsData.forEach(async job => {
+                let element = await createJobElement(job);
+                element.fadeOut(0);
+                jobListElement.append(element);
+                element.fadeIn(500);
+            })
+        }
+    })
+})
+
+const loadImage = (src, alt, classes) =>
+    new Promise((resolve, reject) => {
+        const img = new Image();
+        let imageObject = $(img);
+        imageObject.addClass(classes);
+        imageObject.attr('alt', alt);
+        img.onload = () => resolve(imageObject);
+        img.onerror = reject;
+        img.src = src;
+    })
+;
+
+async function createJobElement(job) {
+    let image = await loadImage(job.imageUrl, job.name, "card-img-top");
+
+    let element = $(`<div class="card">` +
+        '<div class="card-body">' +
+        `<h5 class="card-text mb-0">${job.name}</h5>` +
+        `<a href="${job.url}" class="stretched-link"></a>` +
+        '</div>' +
+        '</div>');
+    element.prepend(image);
+    return element;
+}
