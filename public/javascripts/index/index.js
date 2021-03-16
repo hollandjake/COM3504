997d0de3ad729b03ac25d893b907fc4e31c2df60
Tom-Boyd <thomasboyd2013@hotmail.co.uk>
2021-03-16 16:07:27 +0000

    Added a plus button which opens a modal with a template form

--- public/javascripts/index/index.js
+++ public/javascripts/index/index.js
@@ -32,6 +32,16 @@ $(function () {
             }
         }
     })
+
+    // Used in the form to add a job to switch between uploading an image and entering the URL
+    $('#useImageURL').change(function(){
+        $('#inputImageURLGroup').toggleClass('d-block d-none');
+        $('#inputImageUploadGroup').toggleClass('d-none d-block');
+    });
+    $('#useImageUpload').change(function(){
+        $('#inputImageURLGroup').toggleClass('d-none d-block');
+        $('#inputImageUploadGroup').toggleClass('d-block d-none');
+    });
 })
 
 //Preloads the image so that the browser doesnt reflow the content
