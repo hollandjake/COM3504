0018f30eac15b9fd17338895cd697be8d5ca0b74
acb18wb <wbabbage1@sheffield.ac.uk>
2021-03-23 13:31:25 +0000

    added image carousel
    closes #12

--- routes/job.js
+++ routes/job.js
@@ -7,4 +7,10 @@ router.get('/list',  async function(req, res) {
     res.send(await jobs.getAll());
 });
 
+router.get('/:jobId',  async function( req, res) {
+    let job = await jobs.get(req.params['jobId']);
+    res.render('job', {title: `Job - ${job.name}`,job: job});
+})
+
+
 module.exports = router;
