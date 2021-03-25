428fe0dc0a6de0d2cc07be3a762ed5e14ac11fa5
Jake Holland <hollandjake@hotmail.co.uk>
2021-03-25 17:47:21 +0000

    Added network configuration for the annotation system

--- public/javascripts/job/annotate.js
+++ public/javascripts/job/annotate.js
@@ -15,6 +15,20 @@ export default class Annotate {
         return this._draw;
     }
 
+    get eventNode() {
+        return this._draw.node;
+    }
+
+    static generateUID() {
+        // I generate the UID from two parts here
+        // to ensure the random number provide enough bits.
+        let firstPart = (Math.random() * 46656) | 0;
+        let secondPart = (Math.random() * 46656) | 0;
+        firstPart = ("000" + firstPart.toString(36)).slice(-3);
+        secondPart = ("000" + secondPart.toString(36)).slice(-3);
+        return firstPart + secondPart;
+    }
+
     async init() {
         const [container, draw, imageElement, imageSize] = await this.createCanvas(this._image, this._imageClasses, this._containerClasses);
         this._container = container;
@@ -23,10 +37,9 @@ export default class Annotate {
         this._nativeResolution = imageSize;
         this._renderResolution = null;
         this._is_drawing = false;
-        this._my_active_drawing = null;
-        this._offset = { x: window.pageXOffset, y: window.pageYOffset };
-
-        this.checkSize();
+        this._my_active_id = null;
+        this._network_elements = {};
+        this._offset = {x: window.pageXOffset, y: window.pageYOffset};
         this.initEvents();
         return this;
     }
@@ -52,11 +65,16 @@ export default class Annotate {
         let node = this._draw.node;
         try {
             node.addEventListener('mousedown', (e) => annotation.startDrawing(e));
-            node.addEventListener('touchstart',(e) => annotation.startDrawing(e));
-            node.addEventListener('mouseup',(e) => annotation.endDrawing(e));
-            node.addEventListener('touchend',(e) => annotation.endDrawing(e));
-            node.addEventListener('mousemove',(e) => annotation.onDrag(e));
+            node.addEventListener('touchstart', (e) => annotation.startDrawing(e));
+            node.addEventListener('mouseup', (e) => annotation.endDrawing(e));
+            node.addEventListener('mouseleave', (e) => annotation.endDrawing(e));
+            node.addEventListener('touchend', (e) => annotation.endDrawing(e));
+            node.addEventListener('touchcancel', (e) => annotation.endDrawing(e));
+            node.addEventListener('mousemove', (e) => annotation.onDrag(e));
             node.addEventListener('resize', (e) => annotation.updateSize());
+            this.eventNode.addEventListener('startDrawing', (e) => annotation.onNetworkEvent(e));
+            this.eventNode.addEventListener('endDrawing', (e) => annotation.onNetworkEvent(e));
+            this.eventNode.addEventListener('onDraw', (e) => annotation.onNetworkEvent(e));
         } catch (e) {
             console.log(e);
         }
@@ -67,38 +85,66 @@ export default class Annotate {
             this._is_drawing = true;
             const point = this.getPoint(e);
             let initialPoint = [[point.x, point.y]];
-            this._offset = { x: window.pageXOffset, y: window.pageYOffset };
+            this._offset = {x: window.pageXOffset, y: window.pageYOffset};
+            this._my_active_id = Annotate.generateUID();
 
-            this._my_active_drawing = {
-                id: 1,
-                data: initialPoint,
-                element: this._draw.polyline(initialPoint).fill('none').stroke({ width: 10 }),
-                unprocessed_data: [],
-            };
-            this.onDraw(this._my_active_drawing);
-            console.log('yee');
+            this.eventNode.dispatchEvent(new CustomEvent('startDrawing', {
+                detail: {
+                    type: 'start',
+                    id: this._my_active_id,
+                    data: initialPoint
+                }
+            }));
         }
     }
 
     endDrawing(e) {
         if (this._is_drawing) {
             this._is_drawing = false;
-            console.log('booo')
+            this.eventNode.dispatchEvent(new CustomEvent('endDrawing', {
+                detail: {
+                    type: 'end',
+                    id: this._my_active_id,
+                    data: this._network_elements[this._my_active_id]
+                }
+            }));
         }
     }
 
     onDrag(e) {
         if (this._is_drawing) {
             const point = this.getPoint(e);
-            this._my_active_drawing.unprocessed_data.push([point.x, point.y])
-            this.onDraw(this._my_active_drawing);
+            let data = this._network_elements[this._my_active_id].data;
+            data.push([point.x, point.y]);
+            this.eventNode.dispatchEvent(new CustomEvent('onDraw', {
+                detail: {
+                    type: 'draw',
+                    id: this._my_active_id,
+                    data: data
+                }
+            }));
         }
     }
 
-    onDraw(delta) {
-        delta.unprocessed_data.forEach(d => delta.data.push(d));
-        delta.element.plot(delta.data);
-        delta.unprocessed_data = [];
+    onNetworkEvent(event) {
+        const type = event.detail.type;
+
+        if (type === 'end') {
+            this._network_elements[event.detail.id] = null;
+        } else if (type === 'start') {
+            this._network_elements[event.detail.id] = {
+                element: this._draw.polyline(event.detail.data).fill('none').stroke({width: 10, color: "red"}),
+                data: event.detail.data
+            };
+            this.onDraw(this._network_elements[event.detail.id]);
+        } else if (type === 'draw') {
+            this._network_elements[event.detail.id].data = event.detail.data;
+            this.onDraw(this._network_elements[event.detail.id]);
+        }
+    }
+
+    onDraw(lineData) {
+        lineData.element.plot(lineData.data);
     }
 
     getPoint(e) {
@@ -112,6 +158,5 @@ export default class Annotate {
     updateSize() {
         let node = this._imageElement.node;
         this._renderResolution = node.getBoundingClientRect();
-        console.log(this._renderResolution);
     }
 }
