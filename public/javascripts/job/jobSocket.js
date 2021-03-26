fc07e7094cbd55935af48a109b504008eba093a2
acb18wb <wbabbage1@sheffield.ac.uk>
2021-03-26 00:17:26 +0000

    base chatbox functionality

--- public/javascripts/job/jobSocket.js
+++ public/javascripts/job/jobSocket.js
@@ -5,8 +5,25 @@ const job = io.connect('/job');
 $(function () {
     //Event for when a new image has been added
     job.on('newImage', newImageAdded)
+
+
+    joinJob(7);
+    job.on('chat', function (jobID, message) {
+        writeOnChatHistory(message);
+    });
+
 })
+
+
 
 export function joinJob(jobID) {
     job.emit('join', jobID);
+}
+
+function writeOnChatHistory(message) {
+    let history = document.getElementById('chatboxmsg');
+    let paragraph = document.createElement('tr');
+    paragraph.innerHTML = "\n<th scope=\"row\">Tom:</th>\n<td class=\"w-100\">"+message+"</td>\n";
+    history.appendChild(paragraph);
+    document.getElementsByName('chatmessage')[0].value = '';
 }
--- socket.io/socket-io.js
+++ socket.io/socket-io.js
@@ -1,5 +1,12 @@
+const createJob = require("./createJob.js");
+const addImage = require("./addImage.js");
+
 exports.init = function(io) {
   io.of('/job').on('connection', function(socket) {
+    socket.on('create', (data) => createJob(io, socket, data));
     socket.on('join', (jobID) => socket.join(jobID));
+    socket.on('chat', function(jobID, message) {
+      io.of('/job').to(7).emit('chat', jobID, message);
+    });
   });
 }
