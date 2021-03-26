b18a0726e1940e5d59a3002670d66c7275744c0d
acb18wb <wbabbage1@sheffield.ac.uk>
2021-03-26 14:47:49 +0000

    js functionality to job views page

---
+++ public/javascripts/job/chat.js
@@ -0,0 +1,17 @@
+const job = io.connect('/job');
+let currentPage = 0
+
+function sendChat() {
+    let jobID = parseInt(JOB_ID);
+    let userID = document.getElementById('nav-name').innerHTML;
+    let message = document.getElementsByName('chatmessage')[currentPage].value;
+    job.emit('chat', jobID, userID, message, currentPage);
+}
+
+function nextJob() {
+currentPage += 1;
+}
+
+function prevJob() {
+currentPage -= 1;
+}
