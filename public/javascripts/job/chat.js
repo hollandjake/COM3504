cd86d64276b52ae24823841bca3ae3de63aa3061
acb18wb <wbabbage1@sheffield.ac.uk>
2021-04-09 14:39:38 +0100

    didnt originally commit chat file

---
+++ public/javascripts/job/chat.js
@@ -0,0 +1,20 @@
+const job = io.connect('/job');
+
+function sendChat(image_id) {
+    let jobID = parseInt(JOB_ID);
+    let userID = document.getElementById('nav-name').innerHTML;
+    let message = document.getElementById('message'+image_id).value;
+
+    job.emit('chat', jobID, userID, message, image_id);
+}
+
+function sendEnterChat(image_id, e) {
+    if(e.key == "Enter") {
+        let jobID = parseInt(JOB_ID);
+        let userID = document.getElementById('nav-name').innerHTML;
+        let message = document.getElementById('message'+image_id).value;
+
+        job.emit('chat', jobID, userID, message, image_id);
+    }
+
+}
