4d896534948d6d818b2fc9b2576e8ff463b743ca
acb18wb <wbabbage1@sheffield.ac.uk>
2021-04-09 17:54:34 +0100

    edited passing of mouse event, startDrawing now works

--- public/javascripts/job/job.js
+++ public/javascripts/job/job.js
@@ -96,9 +96,15 @@ function updateCarouselArrows() {
     }
 }
 
+let i = 1;
+
 async function createImageElement(image) {
     const annotation = await new Annotate(image, "card-img-top", "card-img-top job-image").init();
 
+    if (i == 1) {
+        console.log(annotation);
+    }
+    i+=1;
     annotations.push(annotation);
 
     let imageElement = $(`
