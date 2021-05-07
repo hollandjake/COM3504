d065177c1e688bf34a5cd81402a4f8719f0f8646
acb18wb <wbabbage1@sheffield.ac.uk>
2021-05-07 14:49:06 +0100

    moved window online logic to proper place

--- public/javascripts/index/index.js
+++ public/javascripts/index/index.js
@@ -1,5 +1,5 @@
 import {loadImage} from "../components/preloadImage.js";
-import {getPID, getJobs, saveJob, getImage, pushingToServer} from "../databases/database.js";
+import {getPID, getJobs, saveJob, getImage} from "../databases/database.js";
 import {error} from "../components/error.js";
 import {getModalData} from "../components/modal.js";
 
@@ -7,8 +7,6 @@ let loadedJobs = {};
 
 // On load
 $(async function () {
-    await pushingToServer();
-
     //Ajax call to get the list of jobs
     let currentlyRunningAddJobCallback = null;
     await getJobs(async (jobsData) => {
